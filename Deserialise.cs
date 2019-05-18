8803e1a7d2042e88283f11b875a25a0a3a3e92f7
Evelyn <evc3355@gmail.com>
2019-05-18 09:51:13 +0100

    Update Deserialise.cs

--- Deserialise.cs
+++ Deserialise.cs
@@ -1,4 +1,6 @@
-string json = File.ReadAllText(@"{some JSON");
+using Newtonsoft.Json;
+
+string json = @"{some JSON}";
 var json1 = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, object>>>(json);
 var json2 = json1["data"]["quote"].ToString();
 var json3 = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, object>>>(json2);
@@ -6,7 +8,8 @@ var json4 = json3["ETH"]["price"];
 Console.WriteLine((double)json4);
 Console.ReadKey();
 
-The JSON is irregular:
+// The JSON below is irregular, such that "status" and "data" are not the same, hence the use of the boxed variable object
+// "data" has a nested object/dictionary, "quote" hence the second deserialisation:
 {
     "status": {
         "timestamp": "2019-05-17T16:11:00.511Z",
