a54d0c4541944e9d5032f836e1ac2c053ad5d078
Evelyn <evc3355@gmail.com>
2019-05-18 10:07:54 +0100

    Update Deserialise.cs

--- Deserialise.cs
+++ Deserialise.cs
@@ -2,10 +2,13 @@ using Newtonsoft.Json;
 
 string json = @"{some JSON}";
 var json1 = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, object>>>(json);
+// Json1 is now a dictionary, cast it to a string next
 string json2 = json1["data"]["quote"].ToString(); // This will yield the "ETH" object
+// Repeat with the ETH object
 Dictionary<string, Dictionary<string, object>> json3 = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, object>>>(json2);
-double json4 = (double)json3["ETH"]["price"]; // This will get me the target, the price of 0.438
-Console.WriteLine(json4);
+// Json3 is a nested dictionary which can be accessed as normal. 
+double target = (double)json3["ETH"]["price"]; // This will get me the target, the price of 0.438
+Console.WriteLine(target);
 Console.ReadKey();
 
 // The JSON below is irregular, such that "status" and "data" are not the same, hence the use of the boxed variable object
